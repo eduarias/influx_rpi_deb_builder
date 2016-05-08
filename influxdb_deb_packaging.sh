94067c3cc489c37f4f9bd889ab20da894a335cc4
eduarias <eduarias@gmail.com>
2016-05-09 01:10:53 +0200

    Extract package version from deb creation scripts

--- influxdb_deb_packaging.sh
+++ influxdb_deb_packaging.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+VERSION=0.12.2-1
 
 ### --- Package InfluxDB for RPi in a deb package
-URL_BASE='https://s3.amazonaws.com/influxdb/'
-FILE='influxdb-0.12.1-1_linux_armhf.tar.gz'
-TEMP_FOLDER='/tmp/influxdb_0.12.1-1/'
-DEB_PACKAGE='influxdb_0.12.1-1.deb'
-DEBIAN_FOLDER='deb_packaging/debian_influxdb'
+URL_BASE=https://s3.amazonaws.com/influxdb/
+FILE=influxdb-${VERSION}_linux_armhf.tar.gz
+TEMP_FOLDER=/tmp/influxdb_${VERSION}/
+DEB_PACKAGE=influxdb_${VERSION}.deb
+DEBIAN_FOLDER=deb_packaging/debian_influxdb
 
 # Call packaging script
 source deb_packaging_base
--- telegraf_deb_packaging.sh
+++ telegraf_deb_packaging.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+VERSION=0.12.1-1
 
 ### --- Package Telegraf for RPi in a deb package
-URL_BASE='http://get.influxdb.org/telegraf/'
-FILE='telegraf-0.12.0-1_linux_armhf.tar.gz'
-TEMP_FOLDER='/tmp/telegraf-0.12.0-1/'
-DEB_PACKAGE='telegraf-0.12.0-1.deb'
-DEBIAN_FOLDER='deb_packaging/debian_telegraf'
+URL_BASE=http://get.influxdb.org/telegraf/
+FILE=telegraf-${VERSION}_linux_armhf.tar.gz
+TEMP_FOLDER=/tmp/telegraf-${VERSION}/
+DEB_PACKAGE=telegraf-${VERSION}.deb
+DEBIAN_FOLDER=deb_packaging/debian_telegraf
 
 # Call packaging script
 source deb_packaging_base
